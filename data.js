8e47947d551e3fc005e18353ea1c3eeee1d018cf
Rouben Tchakhmakhtchian <rouben@rouben.net>
2021-05-05 19:17:54 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,97 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lm108",
+      "name": "lm108",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.186754825508764,
+        "pitch": 0.061652956292913785,
+        "fov": 1.1907657332996655
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.367005733617672,
+          "pitch": -0.04286845815116713,
+          "title": "RevTouch",
+          "text": "Some revtouch instructions here<div><br></div><div><br></div><div><br></div>"
+        },
+        {
+          "yaw": -1.5485601208777755,
+          "pitch": 0.3060350158717444,
+          "title": "Cables",
+          "text": "HDMI, VGA, USB<div><br></div><div><br></div>"
+        },
+        {
+          "yaw": -1.2139231265609602,
+          "pitch": 0.12200553845116424,
+          "title": "Podium",
+          "text": "Wireless keyboard available here"
+        },
+        {
+          "yaw": -0.9341916441944811,
+          "pitch": 0.09514134226548521,
+          "title": "Whiteboard",
+          "text": "RevTouch pen/stylus kept here"
+        },
+        {
+          "yaw": -0.39029222403226704,
+          "pitch": -0.25670352812609565,
+          "title": "Speaker",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.12367458876154735,
+          "pitch": 0.13843222259460397,
+          "title": "Disinfectant/Cleaning supply",
+          "text": ""
+        },
+        {
+          "yaw": 1.3127177088880089,
+          "pitch": -0.15206280109549652,
+          "title": "Camera and Microphone",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.089100930916059,
+          "pitch": 0.5237566970398735,
+          "title": "Presenter Area",
+          "text": "Lecturer stands here facing the webcam installed opposite the Revtouch"
+        },
+        {
+          "yaw": -2.968005858346025,
+          "pitch": -0.12639656349566408,
+          "title": "Help!",
+          "text": "Contact &lt;&gt; for help"
+        }
+      ]
+    }
+  ],
+  "name": "LM108 AV Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
